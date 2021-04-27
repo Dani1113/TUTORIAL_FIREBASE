5c044653afc5c02f3064aa3decbf09b91fc33c0c
Daniel20091999 <63578276+Daniel20091999@users.noreply.github.com>
2021-04-27 08:46:46 +0200

    empresas casi acabado, producto publicado falta backend

---
+++ app/src/main/java/com/example/tutorialfirebase/Clases/EmpresaViewHolder.java
@@ -0,0 +1,64 @@
+package com.example.tutorialfirebase.Clases;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.tutorialfirebase.Utilidades.ImagenesBlobBitmap;
+
+
+import com.example.tutorialfirebase.MostrarDetalleEmpresaActivity;
+import com.example.tutorialfirebase.R;
+
+import java.io.ByteArrayOutputStream;
+
+public class EmpresaViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public static final String EXTRA_OBJETO_EMPRESA= "com.example.tutorialfirebase.Empresa";
+  //  public static final String EXTRA_IMAGEN_EMPRESA = "com.example.tutorialfirebase.EmpresaViewHolder.imagen_empresa";
+
+    public TextView txt_rv_cod_empresa = null;
+    public TextView txt_rv_clave_empresa = null;
+    public TextView txt_rv_datos_empresa = null;
+    public ImageView img_empresa = null;
+
+    final com.example.tutorialfirebase.Clases.ListaEmpresasAdapter lcAdapter;
+
+    public EmpresaViewHolder(@NonNull View itemView, ListaEmpresasAdapter mAdapter) {
+        super(itemView);
+        txt_rv_cod_empresa = (TextView)  itemView.findViewById(R.id.txt_rv_cod_empresa);
+        txt_rv_clave_empresa = (TextView)  itemView.findViewById(R.id.txt_rv_clave_empresa);
+        txt_rv_datos_empresa = (TextView)  itemView.findViewById(R.id.txt_rv_datos_empresa);
+        img_empresa = (ImageView)  itemView.findViewById(R.id.img_empresa);
+        this.lcAdapter = mAdapter;
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int mPosition = getLayoutPosition();
+        Empresa empresa;
+        empresa = this.lcAdapter.getListaEmpresas().get(mPosition);
+        lcAdapter.notifyDataSetChanged();
+        Intent intent = new Intent(lcAdapter.getC(), MostrarDetalleEmpresaActivity.class);
+        Empresa empresa_sin_imagen = new Empresa(empresa.getCod_empresa(), empresa.getClave_empr(), empresa.getDatos_empr());//, empresa.getIdFoto());
+        intent.putExtra(EXTRA_OBJETO_EMPRESA, (Parcelable) empresa_sin_imagen);
+       // Bitmap foto_empresa_png = empresa.getIdFoto();
+       /* if(foto_empresa_png != null){
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            foto_empresa_png.compress(Bitmap.CompressFormat.PNG, 0, baos);
+            //  foto_empresa_png.compress(Bitmap.CompressFormat.JPEG, 0, baos);
+
+            byte[] byteArrayfoto = baos.toByteArray();
+            intent.putExtra(EXTRA_IMAGEN_EMPRESA, byteArrayfoto);
+        }
+        */
+        lcAdapter.getC().startActivity(intent);
+    }
+}
+
